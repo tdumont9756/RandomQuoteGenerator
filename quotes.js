7d89cb8946d875c63bb39d14cbdb360cc2e620ed
tdumont9756 <tdumont97566@gmail.com>
2017-07-29 07:22:59 -0700

    first code submit

---
+++ quotes.js
@@ -0,0 +1,31 @@
+var quotes = [
+	quote1{
+		author: "Steve Jobs",
+		quote:  "Stay hungry.  Stay foolish"
+	},
+	quote1{
+		author: "Steve Jobs",
+		quote:  "Design is not just what it looks like and feels like. Design is how it works."
+	},
+	quote1{
+		author: "Steve Jobs",
+		quote:  "Sometimes life is going to hit you in the head with a brick. Don't lose faith."
+	},
+	quote1{
+		author: "Steve Jobs",
+		quote:  "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations."
+	},
+	quote1{
+		author: "Steve Jobs",
+		quote:  "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me."
+	},
+	quote1{
+		author: "Steve Jobs",
+		quote:  "Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose. You are already naked. There is no reason not to follow your heart."
+	}
+];
+
+
+function testing(){
+	alert("I'm getting through!");
+}
