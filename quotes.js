b4847f780799a6e549811f5342d2bdf1e4c94b2c
tdumont9756 <tdumont97566@gmail.com>
2017-07-30 13:49:19 -0700

    1)added the on click event for the new quote button

--- quotes.js
+++ quotes.js
@@ -1,26 +1,38 @@
-[
-	quote1{
+var quoteRepo = [
+	{
 		author: "Steve Jobs",
 		quote:  "Stay hungry.  Stay foolish"
 	},
-	quote1{
+	{
 		author: "Steve Jobs",
 		quote:  "Design is not just what it looks like and feels like. Design is how it works."
 	},
-	quote1{
+	{
 		author: "Steve Jobs",
 		quote:  "Sometimes life is going to hit you in the head with a brick. Don't lose faith."
 	},
-	quote1{
+	{
 		author: "Steve Jobs",
 		quote:  "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations."
 	},
-	quote1{
+	{
 		author: "Steve Jobs",
 		quote:  "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me."
 	},
-	quote1{
+	{
 		author: "Steve Jobs",
 		quote:  "Remembering that you are going to die is the best way I know to avoid the trap of thinking you have something to lose. You are already naked. There is no reason not to follow your heart."
+	},
+		{
+		author: "Stephen Curry's manager",
+		quote:  "Are your actions today on par with your dreams for tomorrow?"
+	},
+		{
+		author: "Travis Dumont",
+		quote:  "You are responsible for the energy that you have around you!"
+	},
+		{
+		author: "Stephen Curry's manager",
+		quote:  "Success is not an accident!"
 	}
 ]
--- scripts.js
+++ scripts.js
@@ -1,34 +1,23 @@
-// $(function(){
-// 	$('#new-quote-btn').click(function(){
+$(function(){
 
-// 			$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-//   			$("#the-quote").append(a[0].content + "<p>— " + a[0].title + "</p>");
-// 		});
-// 	});
-	
-// });
+	//default state of page
+	$("#the-quote").text(function(){
+		var text = quoteRepo[0].quote;
+		return text;
+	});
 
 
 
-jQuery( function( $ ) {
-  $( '#new-quote-btn' ).on( 'click', function ( e ) {
-    e.preventDefault();
-    console.log("I know this part works");
-    $.ajax( {
-      url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      success: function ( data ) {
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        //$( '#quote-title' ).text( post.title );
-        $( '#the-quote' ).html( post.content );
-        console.log("the ajax what sussessfull see   " + post.contents);
-        // If the Source is available, use it. Otherwise hide it.
-        if ( typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined' ) {
-          $( '#quote-source' ).html( 'Source:' + post.custom_meta.Source );
-        } else {
-          $( '#quote-source' ).text( '' );
-        }
-      },
-      cache: false
-    } );
-  } );
-} );
+
+	$("#new-quote-btn").click(function(){
+		console.log("the button is clicked");   //for testing only
+		var quoteRepoMax = quoteRepo.length -1; //dyncamicly update max size of the quoteRepo
+		var quoteRepoMin = 0;					//declair magic number
+		$("#the-quote").text(function(){
+				var randomNum = Math.round(Math.random() *(quoteRepoMax - quoteRepoMin));
+				console.log(randomNum);
+				var text = quoteRepo[randomNum].quote;
+				return text;
+		});
+	});
+});
